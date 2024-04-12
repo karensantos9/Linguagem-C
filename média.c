5d8d99c34625c4b0cac36bd3eb95e0b5791d1672
Karen Souza Santos <161501885+karensantos9@users.noreply.github.com>
2024-04-11 22:33:40 -0300

    Add files via upload

---
+++ média.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num =0;
+    int num1 = 0;
+    int num2 = 0;
+    int resultado = 0;
+    
+    printf("digite um número: \n");
+    printf("digite um número: \n");
+    printf("digite um número: \n");
+    scanf("%d",&num);
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    
+    resultado = num*num1*num2/3
+    
+    printf("\no resultado é = %d\n", resultado);
+    return 0;
+}
